7fd30989f997b849113c8c495650ee26b7c4f2f6
doozles411 <cindy.phelps01@gmail.com>
2022-07-20 19:14:41 -0700

    Store tools bar element references

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// Tools-bar functionality
+
+const colorDropper = document.getElementById('color-tool');
+const classicButton = document.getElementById('classic-tool');
+const eraserButton = document.getElementById('eraser-tool');
+const resetButton = document.getElementById('reset-tool');
+const sizeSlider = document.getElementById('sizing-tool');
+const sizeValue = document.getElementById('size-value');
+
+
+
+
+
+
+// Etch-grid functionality
